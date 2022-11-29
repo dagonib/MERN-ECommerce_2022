44bb88e3687101523c8065d22c42d9f8631abb18
dagonib <davidgonzalezibanez@gmail.com>
2022-11-29 13:00:02 +0100

    Video-24-Implement-Signin-API

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import seedRouter from './routes/seedRoutes.js';
 import productRouter from './routes/productRoutes.js';
+import userRouter from './routes/userRoutes.js';
 
 dotenv.config();
 
@@ -20,15 +21,25 @@ mongoose
 // Creación express app. Express retorna un objeto.
 const app = express();
 
+// Convierte los datos en un objeto json.
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 // Aquí se añaden los valores iniciales de Productos y Usuarios.
 app.use('/api/seed', seedRouter);
-
 // El metodo get tiene 2 parametros. el primero es lo que el servidor va a servir y el segundo la función que responde a este API.
 app.use('/api/products', productRouter);
+// Utilización de userRouter.js.
+app.use('/api/users', userRouter);
 
 //Obtención de los datos de la DB datacrypto
 app.get('/api/operations', (req, res) => {
   res.send(datacrypto.operations);
+});
+
+// Gestión de los errores.
+app.use((err, req, res, next) => {
+  res.status(500).send({ message: err.message });
 });
 
 // Información del servidor y conexión a este.
