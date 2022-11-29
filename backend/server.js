feaab94cb305a14304aaec85c7d7dd14f701ba44
dagonib <davidgonzalezibanez@gmail.com>
2022-11-29 11:09:14 +0100

    Video-22-Generate-Sample-Products

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ import data from './data.js';
 import datacrypto from './datacrypto.js';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import seedRouter from './routes/seedRoutes.js';
+import productRouter from './routes/productRoutes.js';
 
 dotenv.config();
 
@@ -18,39 +20,14 @@ mongoose
 // Creación express app. Express retorna un objeto.
 const app = express();
 
+app.use('/api/seed', seedRouter);
+
 // El metodo get tiene 2 parametros. el primero es lo que el servidor va a servir y el segundo la función que responde a este API.
-app.get('/api/products', (req, res) => {
-  // Envía data al fronend.
-  res.send(data.products);
-});
+app.use('/api/products', productRouter);
 
 //Obtención de los datos de la DB datacrypto
 app.get('/api/operations', (req, res) => {
   res.send(datacrypto.operations);
-});
-
-//URL del backend para acceder a la información de un producto.
-app.get('/api/products/slug/:slug', (req, res) => {
-  const product = data.products.find((x) => x.slug === req.params.slug);
-  if (product) {
-    // Envía data al fronend.
-    res.send(product);
-  } else {
-    // Aquí se ponen los mensaje de error.
-    res.status(404).send({ message: 'Product Not Found' });
-  }
-});
-
-//URL del backend para obtener un producto en función del id.
-app.get('/api/products/:id', (req, res) => {
-  const product = data.products.find((x) => x._id === req.params.id);
-  if (product) {
-    // Envía data al fronend.
-    res.send(product);
-  } else {
-    // Aquí se ponen los mensaje de error.
-    res.status(404).send({ message: 'Product Not Found' });
-  }
 });
 
 // Información del servidor y conexión a este.
